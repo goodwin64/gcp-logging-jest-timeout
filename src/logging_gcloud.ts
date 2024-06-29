e02a83a4b0666fc8a3e52eb662824c2aa2f45edc
Max Donchenko <maxx@donchenko@gmail.com>
2024-06-29 14:40:43 +0300

    make jest hang for easy repro

--- src/logging_gcloud.ts
+++ src/logging_gcloud.ts
@@ -1,33 +1,37 @@
 // ❌ makes jest hang
-// import {
-//   Logging as GoogleCloudLogging,
-//   HttpRequest,
-// } from "@google-cloud/logging";
+import {
+  Logging as GoogleCloudLogging,
+  HttpRequest,
+} from "@google-cloud/logging";
 
-// const logging = new GoogleCloudLogging();
+const logging = new GoogleCloudLogging();
+
+// ❌
 
 // ✅ if we just import the type and mock the logger - it doesn't hang:
-import type { HttpRequest } from "@google-cloud/logging";
+// import type { HttpRequest } from "@google-cloud/logging";
 
-const mockLoggerMethod = (
-  _metadata: Record<string, unknown>,
-  _data?: Record<string, unknown> | string
-) => {
-  return {
-    data: {},
-    metadata: {},
-  };
-};
+// const mockLoggerMethod = (
+//   _metadata: Record<string, unknown>,
+//   _data?: Record<string, unknown> | string
+// ) => {
+//   return {
+//     data: {},
+//     metadata: {},
+//   };
+// };
 
-const mockLogger = {
-  info: mockLoggerMethod,
-  entry: mockLoggerMethod,
-};
+// const mockLogger = {
+//   info: mockLoggerMethod,
+//   entry: mockLoggerMethod,
+// };
 
-const logging = {
-  logSync: (_projectId: string) => mockLogger,
-  log: (_projectId: string) => mockLogger,
-};
+// const logging = {
+//   logSync: (_projectId: string) => mockLogger,
+//   log: (_projectId: string) => mockLogger,
+// };
+
+// ✅
 
 // no matter if we use logSync or log - the result is the same
 // export const logger = logging.logSync("test-project-id");
